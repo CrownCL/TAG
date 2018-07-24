51845eafb832750d77c1567069af5c3ed53d84bc
gosts <gosts@AlexJUNG>
2018-07-24 18:06:21 +0900

    Test

--- HelloWorld2/src/HelloWorld2.java
+++ HelloWorld2/src/HelloWorld2.java
@@ -4,6 +4,10 @@ public class HelloWorld2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world!!");
+		System.out.println("hello world!!");
+		System.out.println("hello world!!");
+		System.out.println("hello world!!");
+		System.out.println("hello world!!");
 	}
 
 }
