12d68373969cd89b6ae2c93c45d349913ba26536
gosts <gosts@AlexJUNG>
2018-07-24 17:57:43 +0900

    ddd

--- HelloWorld2/src/HelloWorld2.java
+++ HelloWorld2/src/HelloWorld2.java
@@ -1,10 +1,9 @@
-package test;
 
-public class HelloWorld {
-	
+public class HelloWorld2 {
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("dd");
+		System.out.println("hello world!!");
 	}
 
 }
